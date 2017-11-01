6b1c23ab5d0e0757057c08588f1a73cd6008fe41
Sruli Rapps <srulirapps@gmail.com>
2017-11-01 05:45:21 +0200

    adjust size of control bar icons

--- src/scenes/DailyVibe/components/ControlBar/index.js
+++ src/scenes/DailyVibe/components/ControlBar/index.js
@@ -5,9 +5,9 @@ import styles from './styles';
 
 const ControlBar = () => (
   <View style={styles.container}>
-    <Icon name="ios-arrow-back" size={35} color="white" />
-    <Icon name="ios-pause" size={35} color="white" />
-    <Icon name="ios-arrow-forward" size={35} color="white" />
+    <Icon name="ios-arrow-back" size={30} color="white" />
+    <Icon name="ios-pause" size={30} color="white" />
+    <Icon name="ios-arrow-forward" size={30} color="white" />
   </View>
 );
 
