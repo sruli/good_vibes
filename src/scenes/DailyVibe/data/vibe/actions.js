615942185ed71ee0f300d229069e4519ad0c74ae
Sruli Rapps <srulirapps@gmail.com>
2017-11-02 17:12:29 +0200

    add another two vibes

--- src/scenes/DailyVibe/data/vibe/actions.js
+++ src/scenes/DailyVibe/data/vibe/actions.js
@@ -1,7 +1,8 @@
 import { VIBE_FETCHED } from './actionTypes';
 
 export const vibeFetched = () => {
-  const vibeId = 0;
+  const indexes = [0, 1, 2];
+  const vibeId = indexes[Math.floor(Math.random() * indexes.length)];
   return {
     type: VIBE_FETCHED,
     payload: { vibeId },
--- src/scenes/DailyVibe/data/vibe/api.js
+++ src/scenes/DailyVibe/data/vibe/api.js
@@ -13,6 +13,34 @@ const vibes = [
     ],
     buttonColor: 'black',
   },
+  {
+    id: 2,
+    song: {
+      file: '../../db/vibes/2/disco.mp3',
+      name: 'Don\'t Threaten Me With a Good Time',
+      artist: 'Panic! At The Disco',
+    },
+    images: [
+      require('../../../../../db/vibes/2/1.jpg'),
+      require('../../../../../db/vibes/2/2.jpg'),
+      require('../../../../../db/vibes/2/3.jpg'),
+    ],
+    buttonColor: 'black',
+  },
+  {
+    id: 3,
+    song: {
+      file: '../../db/vibes/3/Golden.mp3',
+      name: 'Golden (feat. Sia)',
+      artist: 'Travie McCoy',
+    },
+    images: [
+      require('../../../../../db/vibes/3/1.jpg'),
+      require('../../../../../db/vibes/3/2.jpg'),
+      require('../../../../../db/vibes/3/3.jpg'),
+    ],
+    buttonColor: 'black',
+  },
 ];
 
 export default vibes;
